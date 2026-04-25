d8234b855516b713f9ec3e447d3fc5066cccd8f4
Alp Efe Karalar <karalar.alpefe@gmail.com>
2026-04-25 15:42:39 +0300

    Implement set cell function

--- game_of_life.sh
+++ game_of_life.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
+# Set total row & col count to the dimensions of the terminal
+# Have 2 cols empty to display status etc.
+# cols=$(tput cols)
+# rows=$(($(tput lines) - 2))
+#
+# Right now, setting it manually will work and be easier for us
+rows=24
+cols=96
+echo "Set dimensions to $rows X $cols"
+# initialize grid to all zeros
+grid=($(printf '0%.0s ' $(seq 1 $((rows * cols)))))
 
 # Functıon to the index of an item at a row and column
-# Need to pass the row index, col index, and total cols count
-# in that exact order!
+# Need to pass the row index, col index
 index() {
   local row=$1
   local col=$2
-  local cols=$3
   echo $(($row * $cols + $col))
 }
 
-# result=$(index 2 3 5)
+# Function to set a cell to 1
+# Need to pass the row index, col index
+set_cell() {
+  local r=$1 c=$2
+  grid[$(index $r $c)]=1
+}
+
+# display grid function
+# TODO: implenment
+# display(){
+#   printf '\e[H'
+#   for ((r=0; r<$rows; r++)); do
+#     for ((c=0; c<$cols; c++)); do
+#       cell=${grid[]}
+# }
+
+# result=$(index 2 3)
 # echo $result
 #
 # PSEUDO CODE
@@ -21,3 +46,4 @@ index() {
 #     count neighbors in the CURRENT grid
 #     write result to NEXT grid
 #   copy NEXT grid -> CURRENT GRID
+# LOGIC
