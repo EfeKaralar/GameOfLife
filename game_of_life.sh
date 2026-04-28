4c0a2ce87c3ce51f1d361c9649544d5fcf160e33
Alp Efe Karalar <karalar.alpefe@gmail.com>
2026-04-28 19:10:53 +0300

    Implement custom grid set up

--- game_of_life.sh
+++ game_of_life.sh
@@ -5,8 +5,9 @@
 rows=24
 cols=96
 echo "Set dimensions to $rows X $cols"
-# initialize grid to all zeros
-grid=($(printf '0%.0s ' $(seq 1 $((rows * cols)))))
+
+mode=""
+preset_num=1
 
 ############ HELPER FUNCTIONS #############
 parse_flags() {
@@ -16,12 +17,20 @@ parse_flags() {
       show_help
       exit 0
       ;;
-    -e) mode="empty" ;;
-    -r) mode="random" ;;
+    -e)
+      mode="empty"
+      setup_grid $mode
+      ;;
+    -r)
+      mode="random"
+      setup_grid $mode
+      ;;
+
     -l)
       mode="preset"
       preset_num=$2
       shift
+      setup_grid $mode $preset_num
       ;;
     *)
       show_help
@@ -39,9 +48,9 @@ Usage: ./game_of_life [flag]
   -e        Start with empty grid
   -r        Start with random pattern
   -l [n]    Load preset number n
-              1: Block
-              2: Blinker
-              3: Glider
+              1: Block + Blinker + Glider
+              2: To be implemented... 
+              3: To be implemented  
 EOF
 }
 
@@ -64,6 +73,43 @@ set_cell() {
   local c=$2
   index idx $r $c
   grid[$idx]=1
+}
+
+setup_grid() {
+  local m=$1
+  local p=${2:-1}
+
+  # initialize grid to all zeros
+  grid=($(printf '0%.0s ' $(seq 1 $((rows * cols)))))
+  case $m in
+
+  empty)
+    :
+    ;;
+
+  random)
+    echo "Randomly initializing grid"
+    # TODO: Implement
+    ;;
+
+  preset)
+    case $p in
+    1)
+      # Place a box
+      block grid 10 32
+      # Place a blinker
+      blinker grid 8 2
+      # Place glider centered around row 5, col 10
+      glider grid 5 10
+      ;;
+    *)
+      printf "Please enter a valid preset number"
+      exit 1
+      ;;
+    esac
+    ;;
+  esac
+
 }
 
 # display grid function
@@ -161,22 +207,13 @@ glider() {
   local -n g=$1
   local r=$2 c=$3
   index idx $r $c
-  g[$idx + 1]=1
+  g[$((idx + 1))]=1
   ((r + 1 < rows && c + 2 < cols)) && g[$idx + cols + 2]=1
   ((r + 2 < rows)) && g[$idx + cols + cols]=1
   ((r + 2 < rows && c + 1 < cols)) && g[$idx + cols + cols + 1]=1
   ((r + 2 < rows && c + 2 < cols)) && g[$idx + cols + cols + 2]=1
 
 }
-
-# Place a box
-block grid 10 32
-
-# Place a blinker
-blinker grid 8 2
-
-# Place glider centered around row 5, col 10
-glider grid 5 10
 
 ############# MAIN FUNCTION ###########
 main "$@"
