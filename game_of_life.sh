867ebd0cf6d764e714f3a04c30a7be21f34f044d
Alp Efe Karalar <karalar.alpefe@gmail.com>
2026-04-28 14:02:35 +0300

    Implement display function

--- game_of_life.sh
+++ game_of_life.sh
@@ -36,14 +36,30 @@ set_cell() {
 }
 
 # display grid function
-# TODO: implenment
-# display(){
-#   printf '\e[H'
-#   for ((r=0; r<$rows; r++)); do
-#     for ((c=0; c<$cols; c++)); do
-#       cell=${grid[]}
-# }
+# TODO:
+# 1. Fix Flicker
+# 2. Add dynamic scaling (reach goal)
+display() {
+  printf '\e[H'
+  for ((r = 0; r < $rows; r++)); do
+    for ((c = 0; c < $cols; c++)); do
+      index idx $r $c
+      cell=${grid[$idx]}
+      [[ $cell -eq 1 ]] && printf '#' || printf ' '
+    done
+  done
+}
 
+# Place glider centered around row 5, col 10
+set_cell 5 11
+set_cell 6 12
+set_cell 7 10
+set_cell 7 11
+set_cell 7 12
+
+while true; do
+  display
+done
 # index idx 2 3
 # echo $idx
 #
